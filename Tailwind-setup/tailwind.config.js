7942173b7d25d66b39b71915c81cb6a5c1824189
Nick Westbrook <ntw3001@gmail.com>
2025-08-01 22:33:40 +0900

    done a bit a responsive stuff

--- Tailwind-setup/tailwind.config.js
+++ Tailwind-setup/tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./Tailwind-setup/index.html",
-    "./Tailwind-setup/**/*.html"
+    "./index.html",
+    "./responsive.html"
   ],
   safelist: [
-    'bg-pink-200',
-    'text-red-600',
-    'p-3',
-    'p-32'
   ],
   theme: {
     extend: {}
