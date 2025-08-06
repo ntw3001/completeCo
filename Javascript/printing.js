5391ddc59cfbe606b0f3000e193b16ec51f7f86c
Nick Westbrook <ntw3001@gmail.com>
2025-08-06 23:45:19 +0900

    just the most basic javascript sillies

---
+++ Javascript/printing.js
@@ -0,0 +1,6 @@
+console.log("hello");
+process.stdout.write("printing.js loaded\n");
+
+console.table({city: "Jaipur", country:"India", population: 3000000});
+console.warn("This is a warning message");
+console.error("This is an error message");
