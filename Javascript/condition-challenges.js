5236410723d98fae0aefa33a01f4e318bf64e404
Nick Westbrook <ntw3001@gmail.com>
2025-08-08 23:05:18 +0900

    more simple js

---
+++ Javascript/condition-challenges.js
@@ -0,0 +1,20 @@
+// is a number greater
+
+let num1 = 15;
+let num2 = 10;
+
+if (num1 > num2) {
+  console.log(`${num1} is greater than ${num2}`);
+} else {
+  console.log(`${num1} is not greater than ${num2}`);
+}
+
+// is the array empty
+
+let arr = [];
+
+if (arr.length === 0) {
+  console.log("The array is empty");
+} else {
+  console.log("The array is not empty");
+}
