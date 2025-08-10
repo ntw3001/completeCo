45571b03432deedea231ed1a214ab85e44b0b864
Nick Westbrook <ntw3001@gmail.com>
2025-08-10 23:15:09 +0900

    some basic js array exercises

---
+++ Javascript/array-challenges.js
@@ -0,0 +1,40 @@
+// const teaFlavours = ["green tea", "black tea", "oolong tea"];
+// const firstTea = teaFlavours[0];
+// console.log(firstTea);
+
+// const cities = ["London", "Tokyo", "Paris", "New York"];
+// const thirdCity = cities[2];
+// console.log(thirdCity);
+
+// let teaTypes = ["herbal tea", "white tea", "masala chai"];
+// teaTypes[1] = "jasmine tea"
+// console.log(teaTypes);
+
+// citiesVisited = ["Mumbai", "Sydney"];
+// citiesVisited.push("Berlin");
+// console.log(citiesVisited);
+
+// let teaOrders = ["chai", "iced tea", "matcha", "earl grey"];
+// lastOrder = teaOrders.pop([-1]);
+// console.log(lastOrder);
+
+// let popularTeas = ["green tea", "oolong tea", "chai"];
+// let softCopyTeas = [popularTeas];
+// console.log(softCopyTeas);
+
+// let topCities = ["New York", "Tokyo", "London"];
+// const hardCopyCities = [...topCities];
+// console.log(hardCopyCities);
+
+// let EuropeanCities = ["Berlin", "Madrid", "Rome"];
+// let AsianCities = ["Tokyo", "Beijing", "Seoul"];
+// let WorldCities = [...EuropeanCities, ...AsianCities];
+// console.log(WorldCities);
+
+// let teaMenu = ["masala chai", "oolong tea", "green tea", "earl grey"];
+// menuLength = teaMenu.length;
+// console.log(menuLength);
+
+cityBucketList = ["New York", "Tokyo", "London", "Cape Town"];
+isLondonInList = cityBucketList.includes("London");
+console.log(isLondonInList);
