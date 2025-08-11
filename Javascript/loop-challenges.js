0d9123cf7a4a6eeed42999e68367798757cbce7c
Nick Westbrook <ntw3001@gmail.com>
2025-08-11 22:26:10 +0900

    my city has been captured

---
+++ Javascript/loop-challenges.js
@@ -0,0 +1,57 @@
+// let sum = 0;
+// let i = 1;
+
+// while (i <= 5) {
+//   sum += i;
+//   i++;
+// }
+
+// console.log("The sum is: " + sum);
+
+// let countdown = [];
+
+// while (!countdown.includes(1)) {
+//   countdown.push(5 - countdown.length);
+// }
+
+// console.log(countdown)
+
+// let guzzleClub = [];
+// let answer;
+
+// do {
+//   answer = prompt("Friend do tell your favrite guzzle" || "")
+
+//   if(answer.toLowerCase() !== "oolong tea will solve the puzzle") {
+//     guzzleClub.push(answer)
+//   }
+// } while (answer.toLowerCase() !== "oolong tea will solve the puzzle")
+
+// let total = 0;
+// let i = 1;
+
+// do{
+//   total += i;
+//   i++;
+// } while (i <= 3);
+
+// console.log("The total is: " + total);
+
+// let multipliedNumbers = [];
+// let numbers = [2, 4, 6];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   isoNum = numbers[i];
+//   multipliedNumbers.push(isoNum * 2);
+// }
+
+// console.log(multipliedNumbers);
+
+let cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+let cityList = [];
+
+for (let i=0; i < cities.length; i++) {
+  cityList.push(cities[i]);
+}
+
+console.log(cityList);
