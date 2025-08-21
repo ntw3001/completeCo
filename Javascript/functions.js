55518bf938732c038240a53c73b279c8ad42dd0d
Nick Westbrook <ntw3001@gmail.com>
2025-08-22 08:28:56 +0900

    more functions# #

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -5,12 +5,32 @@
 // makeTea("guzzleyum")
 // console.log(teaOrder);
 
-function orderTea(teaStyle) {
-  function confirmOrder() {
-    console.log(`Order confirmed: ${teaStyle} tea`);
-  }
-  confirmOrder();
-}
+// function orderTea(teaStyle) {
+//   function confirmOrder() {
+//     console.log(`Order confirmed: ${teaStyle} tea`);
+//   }
+//   confirmOrder();
+// }
+// teaStyle = "lapsang souchong";
+// orderTea(teaStyle);
 
-teaStyle = "lapsang souchong";
-orderTea(teaStyle);
+// const calculateTotal = (price, quantity) => price * quantity;
+// console.log(calculateTotal(3, 106));
+
+// const processTeaOrder = (param) => {
+//   return param("lapsang souchong")
+// }
+// const makeTea = (tea) => {
+//   console.log(`What about my ${tea.toUpperCase()} TEA`)
+// }
+// processTeaOrder (makeTea);
+
+// const createTeaMaker = () => {
+//   return (teaType) => {
+//     return `M-m-making your ${teaType} tea master...`;
+//   }
+// }
+// let teaMaker = createTeaMaker();
+// console.log(teaMaker("Earl Grey"));
+
+
