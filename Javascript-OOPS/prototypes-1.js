2c2ddc189b04c40656933c5bed5012cd297b2342
Nick Westbrook <ntw3001@gmail.com>
2025-09-03 00:16:39 +0900

    　just a tiny but more

--- Javascript-OOPS/prototypes-1.js
+++ Javascript-OOPS/prototypes-1.js
@@ -2,4 +2,12 @@ let computer = {cpu: 12}
 let lenovo = {screen: "HD"}
 let tomHardware = {}
 
-console.log(`computer`, computer.__proto__)
+// console.log(`computer`, computer.__proto__)
+
+let genericCar = {tyres: 4}
+
+let tesla = {drive: "electric"}
+
+Object.setPrototypeOf(tesla, genericCar)
+
+console.log(`tesla`, Object.getPrototypeOf(tesla))
