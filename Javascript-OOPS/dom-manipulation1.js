a8d4bebb341197abc5eb28ca1acde6787ea9a9e6
Nick Westbrook <ntw3001@gmail.com>
2025-09-15 00:26:18 +0900

    dom manipulation more

--- Javascript-OOPS/dom-manipulation1.js
+++ Javascript-OOPS/dom-manipulation1.js
@@ -10,3 +10,26 @@ highlightFirstCity.addEventListener("click", function() {
   let topmostCity = citiesList.firstElementChild
   topmostCity.classList.add("highlight")
 })
+
+const coffeeList = document.getElementById("coffeeOrder")
+const coffeeType = document.getElementById("coffeeType")
+const changeCoffee = document.getElementById("changeOrder")
+changeCoffee.addEventListener("click", function() {
+  coffeeType.textContent = "Espresso"
+})
+
+const addItem = document.getElementById("addNewItem")
+const shopList = document.getElementById("shopList")
+addItem.addEventListener("click", function() {
+  let newItem = document.createElement("li")
+  newItem.textContent = "another 1"
+  shopList.appendChild(newItem)
+})
+
+const removeItem = document.getElementById("removeLastItem")
+const taskList = document.getElementById("taskList")
+removeItem.addEventListener("click", function() {
+  let lastItem = taskList.lastElementChild
+  console.log(lastItem)
+  taskList.removeChild(lastItem)
+})
