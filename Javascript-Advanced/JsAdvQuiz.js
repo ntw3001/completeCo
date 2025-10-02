53a7ee9a2497bb54aa6e5912db59576641d222c8
Nick Westbrook <ntw3001@gmail.com>
2025-10-02 23:57:17 +0900

    test part 2

--- Javascript-Advanced/JsAdvQuiz.js
+++ Javascript-Advanced/JsAdvQuiz.js
@@ -1,35 +1,78 @@
-function simulateAsyncTask() {
-  console.log("Task started")
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(console.log("Task finished"))
-    }, 2000)
-  })
-}
-simulateAsyncTask()
+// function simulateAsyncTask() {
+//   console.log("Task started")
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(console.log("Task finished"))
+//     }, 2000)
+//   })
+// }
+// simulateAsyncTask()
 
-function simulateMultipleTasks() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-        resolve(console.log("Task 1 finished")
-        )
-    }, 1000)
-        setTimeout(() => {
-        resolve(console.log("Task 2 finished")
-        )
-    }, 2000)
-        setTimeout(() => {
-        resolve(console.log("Task 3 finished")
-        )
-    }, 3000)
-})
-}
-simulateMultipleTasks()
+// function simulateMultipleTasks() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//         resolve(console.log("Task 1 finished")
+//         )
+//     }, 1000)
+//         setTimeout(() => {
+//         resolve(console.log("Task 2 finished")
+//         )
+//     }, 2000)
+//         setTimeout(() => {
+//         resolve(console.log("Task 3 finished")
+//         )
+//     }, 3000)
+// })
+// }
+// simulateMultipleTasks()
 
-function fetchDataWithCallback(callback) {
-  setTimeout(() => {
-    const data = "Fetched data"
-    callback(data)
-  }, 2000)
+// function fetchDataWithCallback(callback) {
+//   setTimeout(() => {
+//     const data = "Fetched data"
+//     callback(data)
+//   }, 2000)
+// }
+// fetchDataWithCallback(console.log)
+
+// Create a function createCounter() that returns a function which increments and returns a counter value each time it is called.
+
+function createCounter() {
+  let counter = 0;
+  function increment() {
+    counter ++
+    return counter
+  }
+  return increment
 }
-fetchDataWithCallback(console.log)
+
+// Create a function rateLimiter(fn, limit) that returns a new function. The returned function allows calling fn only once within a limit time in milliseconds. If it is called again before the limit is reached, it should return "Rate limit exceeded".
+
+function rateLimiter(fn, limit) {
+  let available = true
+  return function (...args) {
+    if(!available) {
+      return "Rate limit exceeded"
+    }
+    available = false
+    setTimeout(() => {
+      available = true
+    }, limit)
+    return fn(...args)
+  }
+}
+
+// Write a function memoize(fn) that returns a memoized version of fn. The memoized function should cache the results of function calls, and return the cached result if the same inputs are provided again.
+
+function memoize(fn) {
+  const cache = {}
+
+  return function (...args) {
+    const key = JSON.stringify(args)
+    if (key in cache) {
+      return cache[key]
+    }
+    const result = fn(...args)
+    cache[key] = result
+    return result
+  };
+}
