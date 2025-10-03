f76f5608606d722f0f937cf36e9dc8aba18f26f4
Nick Westbrook <ntw3001@gmail.com>
2025-10-03 23:48:17 +0900

    Not pleased with prototypal inheritance

--- Javascript-Advanced/JsAdvQuiz.js
+++ Javascript-Advanced/JsAdvQuiz.js
@@ -35,44 +35,58 @@
 // fetchDataWithCallback(console.log)
 
 // Create a function createCounter() that returns a function which increments and returns a counter value each time it is called.
-
-function createCounter() {
-  let counter = 0;
-  function increment() {
-    counter ++
-    return counter
-  }
-  return increment
-}
+// function createCounter() {
+//   let counter = 0;
+//   function increment() {
+//     counter ++
+//     return counter
+//   }
+//   return increment
+// }
 
 // Create a function rateLimiter(fn, limit) that returns a new function. The returned function allows calling fn only once within a limit time in milliseconds. If it is called again before the limit is reached, it should return "Rate limit exceeded".
-
-function rateLimiter(fn, limit) {
-  let available = true
-  return function (...args) {
-    if(!available) {
-      return "Rate limit exceeded"
-    }
-    available = false
-    setTimeout(() => {
-      available = true
-    }, limit)
-    return fn(...args)
-  }
-}
+// function rateLimiter(fn, limit) {
+//   let available = true
+//   return function (...args) {
+//     if(!available) {
+//       return "Rate limit exceeded"
+//     }
+//     available = false
+//     setTimeout(() => {
+//       available = true
+//     }, limit)
+//     return fn(...args)
+//   }
+// }
 
 // Write a function memoize(fn) that returns a memoized version of fn. The memoized function should cache the results of function calls, and return the cached result if the same inputs are provided again.
+// function memoize(fn) {
+//   const cache = {}
 
-function memoize(fn) {
-  const cache = {}
+//   return function (...args) {
+//     const key = JSON.stringify(args)
+//     if (key in cache) {
+//       return cache[key]
+//     }
+//     const result = fn(...args)
+//     cache[key] = result
+//     return result
+//   };
+// }
 
-  return function (...args) {
-    const key = JSON.stringify(args)
-    if (key in cache) {
-      return cache[key]
-    }
-    const result = fn(...args)
-    cache[key] = result
-    return result
-  };
+// Task 1
+function Animal(name) {
+  this.name = name
 }
+
+Animal.prototype.makeSound = function() {
+  return "Animal sound"
+
+}
+function Dog(name) {
+  this.bark = function() {
+    return "Woof!"
+  }
+}
+Dog.prototype = Object.create(Animal.prototype)
+Dog.prototype.constructor = Dog
