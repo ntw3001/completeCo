66c0b26a71c2b473cb3fcaafbb4c61f9ace4e53e
Nick Westbrook <ntw3001@gmail.com>
2025-10-14 23:19:06 +0900

    just another task list

---
+++ 01_todo-localstorage/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input")
+  const addTaskButton = document.getElementById("add-task-btn")
+  const todoList = document.getElementById("todo-list")
+
+  // Either retrieve the JSON that was stringified and stored in the saveTasks function, or create a new empty array
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || []
+
+  tasks.forEach(task => renderTask(task))
+
+  addTaskButton.addEventListener("click", () => {
+    const taskText = todoInput.value.trim()
+    if(taskText === "") return
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false
+    }
+    tasks.push(newTask)
+    todoInput.value = ""
+    console.log(tasks)
+  })
+
+  function renderTask(task) {
+    console.log("fick!")
+  }
+
+  // Stringify the task list and save to local storage
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }
+
+})
