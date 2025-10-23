2592be8e645606eb296c79069fa6b73aa147b43c
Nick Westbrook <ntw3001@gmail.com>
2025-10-23 23:56:36 +0900

    got that weather app going

--- 02_weather/script.js
+++ 02_weather/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const descriptionDisplay = document.getElementById("description");
   const errorMessage = document.getElementById("error-message");
 
-  const apiKey = ""
+  const apiKey = "
 
   getWeatherBtn.addEventListener("click", async () => {
     const city = cityInput.value.trim();
@@ -15,17 +15,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
       const weatherData = await fetchWeatherData(city)
+      displayWeatherData(weatherData)
     } catch (error) {
       showError()
     }
   })
 
   async function fetchWeatherData(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    const response = await fetch(url)
 
+    if (!response.ok) {
+      throw new Error("No such city exists!")
+    }
+
+    const json = await response.json()
+    return json
   }
 
   function displayWeatherData(weatherData) {
-
+    console.log(weatherData)
+    weatherInfo.classList.remove("hidden")
+    errorMessage.classList.add("hidden")
+    const { name, main, weather } = weatherData
+    cityNameDisplay.textContent = name
+    temperatureDisplay.textContent = `Temperature: ${main.temp} °C`
+    descriptionDisplay.textContent = `Weather: ${weather[0].description}`
   }
 
   function showError() {
