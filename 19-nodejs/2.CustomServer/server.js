f048beb615bc06059c84125c7137e1343a696cba
Nick Westbrook <ntw3001@gmail.com>
2025-11-20 00:11:01 +0900

    some nice things with custom servers

--- 19-nodejs/2.CustomServer/server.js
+++ 19-nodejs/2.CustomServer/server.js
@@ -20,6 +20,23 @@ const server = http.createServer((req, res) => {
     ".jpg": "image/jpg",
   }
   mimeTypes[extensionName] || "application/octet-stream";
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code === "ENOENT") {
+        fs.readFile("./404.html", (error, content) => {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end(content, "utf-8");
+        });
+      } else {
+        res.writeHead(500);
+        res.end(`Sorry, check with the site admin for error: ${error.code} ..\n`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": mimeTypes[extensionName] || "application/octet-stream" });
+      res.end(content, "utf-8");
+    }
+  });
 });
 
 
