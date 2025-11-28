8098678312523ac8b9878c90c3222ce036a89451
Nick Westbrook <ntw3001@gmail.com>
2025-11-28 23:43:00 +0900

    just added the files for the MongoDB section

--- 19-nodejs/3-eventLogger/logger.js
+++ 19-nodejs/3-eventLogger/logger.js
@@ -4,7 +4,7 @@ import os from "os";
 const EventEmitter = require("events");
 
 class Logger extends EventEmitter {
-  log(message) {
+  log(message) {w
     this.emit("message", { message });
   }
 }
@@ -12,12 +12,12 @@ class Logger extends EventEmitter {
 const logger = new Logger();
 const logFile = "./eventlog.txt";
 
-const logToFIle = (event) => {
+const logToFile = (event) => {
   const logMessage = `${new Date().toISOString()} - ${event.message} \n`;
   fs.appendFileSync(logFile, logMessage);
 }
 
-logger.on("message", logToFIle);
+logger.on("message", logToFile);
 
 setInterval(() => {
   const memoryUsage = (os.freemem() / os.totalmem()) * 100;
