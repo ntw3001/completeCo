9b8284a0bf95d09e9049d123e1dcedb6f6a250f4
Nick Westbrook <ntw3001@gmail.com>
2025-12-01 23:27:06 +0900

    set up folder structure and a couple of  routes for the database experiment

--- projmng/index.js
+++ projmng/index.js
@@ -1,1 +1,21 @@
-console.log("Project Management Module Loaded");
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config({
+  path: './.env'
+});
+
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.send('alright alright')
+})
+
+app.get('/instagram', (req, res) => {
+  res.send('In stag ram? What?')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
