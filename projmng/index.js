07b5878a0932677ee04eaae80d11f4c0079a65a7
Nick Westbrook <ntw3001@gmail.com>
2025-12-02 23:56:09 +0900

    separated index.js from app.js

--- projmng/index.js
+++ projmng/index.js
@@ -1,20 +1,11 @@
 import dotenv from 'dotenv';
-import express from 'express';
+import app from './app.js';
 
 dotenv.config({
   path: './.env'
 });
 
-const app = express()
 const port = 3001
-
-app.get('/', (req, res) => {
-  res.send('alright alright')
-})
-
-app.get('/instagram', (req, res) => {
-  res.send('In stag ram? What?')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
