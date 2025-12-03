0fd6b2a3f3d00039ff13ac77ba723d8d825dcc70
Nick Westbrook <ntw3001@gmail.com>
2025-12-03 22:43:58 +0900

    configured cors

--- projmng/app.js
+++ projmng/app.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express()
+
+app.use(express.json({ limit: '16kb' }))
+app.use(express.urlencoded({ extended: true, limit: '16kb' }))
+app.use(express.static('public'))
+
+// cors config
+app.use(cors({
+  origin: process.env.CORS_ORIGIN.split(',') || "http://localhost:3001",
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedGHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.get('/', (req, res) => {
   res.send('alright alright yes I\'m here')
