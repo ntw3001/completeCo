760f50ea9e62df1b4b9f84df8a5e0e6fedb727d1
Nick Westbrook <ntw3001@gmail.com>
2025-12-10 23:32:39 +0900

    set up connection to mongodb

--- projmng/index.js
+++ projmng/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import connectDB from './db/index';
 
 dotenv.config({
   path: './.env'
@@ -7,6 +8,13 @@ dotenv.config({
 
 const port = 3001
 
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    })
+  })
+  .catch((err) => {
+    console.error('Failed to connect to the database', err)
+    process.exit(1)
+  });
