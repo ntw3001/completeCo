d3ed8f4315bf45aeabac74e6c9ab31112eb14c46
Nick Westbrook <ntw3001@gmail.com>
2025-12-15 23:08:06 +0900

    set up async handling

--- projmng/src/controllers/healthcheck.controller.js
+++ projmng/src/controllers/healthcheck.controller.js
@@ -1,12 +1,12 @@
 import { ApiResponse } from '../utils/api-response.js';
+import { asyncHandler } from '../utils/async-handler.js';
 
-const healthcheck = (req, res) => {
-  try {
-    res.status(200).json(
-      new ApiResponse(200, {
-        message: "Server is go"
-      }))
-  } catch (error) {}
-}
+const healthCheck = asyncHandler( async (req, res) => {
+  res.status(200).json(
+    new ApiResponse(200, {
+      message: "Server is go"
+    })
+  )
+});
 
-export { healthcheck }
+export { healthCheck }
