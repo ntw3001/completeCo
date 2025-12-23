41f565d1fde88f329a79a72154334666b0bd58f5
Nick Westbrook <ntw3001@gmail.com>
2025-12-23 23:59:47 +0900

    set up automated email

--- projmng/src/utils/mail.js
+++ projmng/src/utils/mail.js
@@ -1,6 +1,42 @@
 import Mailgen from 'mailgen';
+import nodeMailer from 'nodemailer';
 
-const mailgenVerificarionEmail = (username, verificationUrl) => {
+const sendEmail = async (options) => {
+  const mailGenerator = new Mailgen({
+    theme: 'default',
+    product: {
+      name: '3001 Industries Operational Function',
+      link: 'https://3001-industries.com/',
+    },
+  })
+  const mailText = mailGenerator.generatePlaintext (options.mailgenContent)
+  const mailHTML = mailGenerator.generate (options.mailgenContent)
+
+  const transporter = nodeMailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_SMTP_USER,
+      pass: process.env.MAILTRAP_SMTP_PASS,
+    },
+  });
+  const mail = {
+    from: '"3001 Industries" <no-reply@3001-industries.com>',
+    to: options.email,
+    subject: options.subject,
+    text: mailText,
+    html: mailHTML,
+  };
+
+  try {
+    await transporter.sendMail(mail);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
+
+const mailgenVerificationEmail = (username, verificationUrl) => {
   return {
     body: {
       name: username,
@@ -24,7 +60,7 @@ const mailgenPasswordForgetEmail = (username, passwordResetUrl) => {
       name: username,
       intro: 'Yeah some dummy forgot their email. Wait that was you? Oh I mean uh, some dummy forgot your email',
       action: {
-        instruction: 'touch the buton to remember it then dummy',
+        instruction: 'Touch the buton to remember it then dummy',
         button: {
           color: '#830101ff',
           text: 'I am ashamed',
@@ -33,3 +69,4 @@ const mailgenPasswordForgetEmail = (username, passwordResetUrl) => {
       }
     }
   }
+}
