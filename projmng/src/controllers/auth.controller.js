84a816aa92cdc1a2c3f95f1f649e990b2449c1ca
Nick Westbrook <ntw3001@gmail.com>
2026-01-08 23:58:03 +0900

    some work on le logout power

--- projmng/src/controllers/auth.controller.js
+++ projmng/src/controllers/auth.controller.js
@@ -111,4 +111,22 @@ const loginUser = asyncHandler(async (req, res) => {
       )
 });
 
-export { registerUser, loginUser }
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: ""
+      }
+    },
+    {
+      new: true,
+    }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+});
+
+export { registerUser, loginUser, logoutUser };
--- projmng/src/routes/auth.routes.js
+++ projmng/src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { registerUser, loginUser } from '../controllers/auth.controller.js';
+import { registerUser, loginUser, logoutUser } from '../controllers/auth.controller.js';
 import { userRegisterValidator, userLoginValidator } from '../validators/index.js';
 import { validate } from '../middleware/validator.middleware.js';
+import { verifyJWT } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
 router.route('/register').post(userRegisterValidator(), validate, registerUser);
 router.route('/login').post(userLoginValidator(), validate, loginUser);
+router.route('/logout').post(verifyJWT, logoutUser);
 
 export default router;
