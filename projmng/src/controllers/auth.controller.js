3f58ebcd9de0de08a2a78dcb2ff5a6a7b6ceffb3
Nick Westbrook <ntw3001@gmail.com>
2026-01-08 23:59:28 +0900

    oops finished that up

--- projmng/src/controllers/auth.controller.js
+++ projmng/src/controllers/auth.controller.js
@@ -126,7 +126,18 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-  };
+  }
+  return res
+    .status(200
+    .clearCookie('accessToken', options)
+    .clearCookie('refreshToken', options)
+    .json(
+      new ApiResponse(
+        200,
+        {},
+        'User logged out successfully'
+      )
+    )
 });
 
 export { registerUser, loginUser, logoutUser };
