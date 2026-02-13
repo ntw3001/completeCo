6e1cf1ca388e90c31e67b880430a28b7dc58d57f
Nick Westbrook <ntw3001@gmail.com>
2026-02-13 23:53:22 +0900

    hooked it all up to mongodb

--- 22-mega-project/src/index.js
+++ 22-mega-project/src/index.js
@@ -1,1 +1,21 @@
-console.log("weeeey");
+
+import dotenv from 'dotenv';
+import { app } from './app.js';
+import { connectDB } from './db/index.js';
+
+dotenv.config({
+  path: "./.env"
+})
+
+const PORT = process.env.PORT || 3001;
+
+connectDB()
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  })
+})
+.catch((err) => {
+  console.error("Failed to connect to the database:", err);
+  process.exit(1);
+})
