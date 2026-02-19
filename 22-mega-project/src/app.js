411160df4e7fd14cdc8bbecdc0c1b101f4249ec0
Nick Westbrook <ntw3001@gmail.com>
2026-02-19 23:36:23 +0900

    healthcheck checked

--- 22-mega-project/src/app.js
+++ 22-mega-project/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import router from './routes/healthcheck.routes.js';
 
 const app = express()
 
@@ -10,7 +11,15 @@ app.use(
   }),
 )
 
+// middleware
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.static('public'));
+
+// import routes
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+
+//routes
+app.use("/api/v1/healthcheck", healthcheckRouter)
+
 export { app };
