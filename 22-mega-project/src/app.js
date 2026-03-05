ab2d3b9a71bacc97aa802a420e2f34564564ec53
Nick Westbrook <ntw3001@gmail.com>
2026-03-05 23:51:28 +0900

    more and more

--- 22-mega-project/src/app.js
+++ 22-mega-project/src/app.js
@@ -15,14 +15,18 @@ app.use(
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.static('public'));
-app.use(cookieParser)
+app.use(cookieParser())
 
 // import routes
 import healthcheckRouter from "./routes/healthcheck.routes.js"
 import userRouter from "./routes/user.routes.js"
+import errorHandler from "./middlewares/error.middlewares.js"
 
 //routes
 app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/user", userRouter)
+
+
+app.use(errorHandler)
 
 export { app };
--- 22-mega-project/src/utils/apiError.js
+++ 22-mega-project/src/utils/apiError.js
@@ -12,11 +12,11 @@ class ApiError extends Error {
     this.success = false
     this.errors = this.errors;
 
-    if (stack) {
-      this.stack = stack
-    } else {
-      Error.captureStackTrace(this, this.constructor)
-    }
+    // if (stack) {
+    //   this.stack = stack
+    // } else {
+    //   Error.captureStackTrace(this, this.constructor)
+    // }
   }
 }
 
