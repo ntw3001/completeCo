428f1ac3fa483d3bbadfa32ee082126f8c563d20
Nick Westbrook <ntw3001@gmail.com>
2026-03-16 23:38:25 +0900

    dealth with refreshing access tokens

--- 22-mega-project/src/controllers/user.controllers.js
+++ 22-mega-project/src/controllers/user.controllers.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.models.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { deleteFromCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/apiResponse.js"
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefreshToken = async (userId) => {
 try {
@@ -155,7 +156,58 @@ const loginUser = asyncHandler( async (req, res) => {
     ))
 })
 
+const logoutUser = asyncHandler (async (req, res) => {
+  await User.findByIdAndUpdate(
+    //not yet
+  )
+})
+
+const refreshAccessToken = asyncHandler (async (req, res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+  if(!incomingRefreshToken) {
+    throw new ApiError(401, "Refresh token is required")
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    )
+    const user = await User.findById(decodedToken?._id)
+
+    if(!user) {
+      throw new ApiError(401, "Invalid refresh token")
+    }
+
+    if (user.refreshToken !== incomingRefreshToken) {
+      throw new ApiError(401, "Invalid refresh token")
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production"
+    }
+
+    const {accessToken, refreshToken: newRefreshToken} =
+      await generateAccessAndRefreshToken(user._id)
+
+      return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+          new ApiResponse(
+            200, ({accessToken, refreshToken: newRefreshToken}, "Access token refreshed :)")
+          )
+        )
+  } catch (error) {
+    throw new ApiError(500, "Refreshing the access token didn't work")
+  }
+})
+
 export {
   registerUser,
-  loginUser
+  loginUser,
+  refreshAccessToken
 }
