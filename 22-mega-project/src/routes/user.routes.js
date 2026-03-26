bca1c59d01e37c12cd45df7acb6d35c206709e7e
Nick Westbrook <ntw3001@gmail.com>
2026-03-26 23:38:57 +0900

    set up routes for all the user services

--- 22-mega-project/src/routes/user.routes.js
+++ 22-mega-project/src/routes/user.routes.js
@@ -19,8 +19,18 @@ router.route("/register").post(
   registerUser
 )
 
+router.route("/login").post(loginUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
 // secured routes
 
 router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
