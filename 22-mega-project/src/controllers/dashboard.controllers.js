6570664f7baeb89d4e3535f0b23fb02642fac127
Nick Westbrook <ntw3001@gmail.com>
2026-04-02 23:53:47 +0900

    dashboard controller done

--- 22-mega-project/src/controllers/dashboard.controllers.js
+++ 22-mega-project/src/controllers/dashboard.controllers.js
@@ -7,62 +7,62 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    const userId = req.user?._id
+  const userId = req.user?._id
 
-    if (!userId) {
-      throw new ApiError(401, "Unauthorized")
-    }
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized")
+  }
 
-    const totalVideos = await Video.countDocuments({ owner: userId })
+  const totalVideos = await Video.countDocuments({ owner: userId })
 
-    const totalSubscribers = await Subscription.countDocuments({
-      channel: userId
-    })
-
-    const viewsData = await Video.aggregate([
-      { $match: { owner: userId } },
-      {
-        $group: {
-          _id: null,
-          totalViews: { $sum: "$views" }
-        }
-      }
-    ])
-    const totalViews = viewsData[0]?.totalViews || 0
-
-    const likesData = await Like.aggregate([
-      {
-        $lookup: {
-          from: "videos",
-          localField: "video",
-          foreignField: "_id",
-          as: "video"
-        }
-      },
-      {
-        $match: {
-          "video.owner": userId
-        }
-      },
-      {
-        $count: "totalLikes"
-      }
-    ])
-    const totalLikes = likesData[0]?.totalLikes || 0
-
-    return res.status(200).json(
-      new ApiResponse(
-        200,
-        {
-          totalVideos,
-          totalSubscribers,
-          totalViews,
-          totalLikes
-        },
-        "Channel stats fetched"
-      )
-    )
+  const totalSubscribers = await Subscription.countDocuments({
+    channel: userId
   })
+
+  const viewsData = await Video.aggregate([
+    { $match: { owner: userId } },
+    {
+      $group: {
+        _id: null,
+        totalViews: { $sum: "$views" }
+      }
+    }
+  ])
+
+  const totalViews = viewsData[0]?.totalViews || 0
+
+  const likesData = await Like.aggregate([
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "video"
+      }
+    },
+    {
+      $match: {
+        "video.owner": userId
+      }
+    },
+    {
+      $count: "totalLikes"
+    }
+  ])
+  const totalLikes = likesData[0]?.totalLikes || 0
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        totalVideos,
+        totalSubscribers,
+        totalViews,
+        totalLikes
+      },
+      "Channel stats fetched"
+    )
+  )
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
