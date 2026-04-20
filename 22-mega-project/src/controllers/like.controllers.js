5b520e43d6373eb2028f643e62d522870611d64a
Nick Westbrook <ntw3001@gmail.com>
2026-04-20 23:32:14 +0900

    completed controllers for likes

--- 22-mega-project/src/controllers/like.controllers.js
+++ 22-mega-project/src/controllers/like.controllers.js
@@ -1,28 +1,165 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose"
+import { Like } from "../models/like.models.js"
+import { Video } from "../models/video.models.js"
+import { Comment } from "../models/comment.models.js"
+import { Tweet } from "../models/tweet.models.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
+  const {videoId} = req.params
+  const userId = req.user?._id
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized")
+  }
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video ID")
+  }
+
+  const video = await Video.findById(videoId)
+
+  if (!video) {
+    throw new ApiError(404, "Video not found")
+  }
+
+  const existingLike = await Like.findOne({
+    likedBy: userId,
+    video: videoId
+  })
+
+  if (existingLike) {
+    await Like.deleteOne({
+      likedBy: userId,
+      video: videoId
+      })
+    return res.status(200).json(
+      new ApiResponse(200, {}, "No longer liked >:[")
+    )
+  }
+
+  await Like.create({
+    likedBy: userId,
+    video: videoId
+  })
+
+  return res.status(200).json(
+    new ApiResponse(200, {}, "Liked :]")
+  )
+
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
+  const {commentId} = req.params
+  const userId = req.user?._id
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized")
+  }
+
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid comment ID")
+  }
+
+  const comment = await Comment.findById(commentId)
+
+  if (!comment) {
+    throw new ApiError(404, "Comment not found")
+  }
+
+  const existingLike = await Like.findOne({
+    likedBy: userId,
+    comment: commentId
+  })
+
+  if (existingLike) {
+    await Like.deleteOne({
+      likedBy: userId,
+      comment: commentId
+      })
+    return res.status(200).json(
+      new ApiResponse(200, {}, "No longer liked >:[")
+    )
+  }
+
+  await Like.create({
+    likedBy: userId,
+    comment: commentId
+  })
+
+  return res.status(200).json(
+    new ApiResponse(200, {}, "Liked :]")
+  )
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+  const {tweetId} = req.params
+  const userId = req.user?._id
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized")
+  }
+
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweet ID")
+  }
+
+  const tweet = await Tweet.findById(tweetId)
+
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found")
+  }
+
+  const existingLike = await Like.findOne({
+    likedBy: userId,
+    tweet: tweetId
+  })
+
+  if (existingLike) {
+    await Like.deleteOne({
+      likedBy: userId,
+      tweet: tweetId
+      })
+    return res.status(200).json(
+      new ApiResponse(200, {}, "No longer liked >:[")
+    )
+  }
+
+  await Like.create({
+    likedBy: userId,
+    tweet: tweetId
+  })
+
+  return res.status(200).json(
+    new ApiResponse(200, {}, "Liked :]")
+  )
+
+})
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+  const userId = req.user?._id
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized")
+  }
+
+  const likedVideos = await Like.find({
+    likedBy: userId,
+    video: { $ne: null, $exists: true }
+  })
+    .populate("video", "title thumbnail owner createdAt")
+    .sort({ createdAt: -1 })
+
+  const videos = likedVideos
+    .map(like => like.video)
+    .filter(Boolean)
+
+  return res.status(200).json(
+    new ApiResponse(200, videos, "Liked videos fetched")
+  )
 })
 
 export {
