2d86bdeec3831a1005285f7ab55dc3a8543bd4d3
Nick Westbrook <ntw3001@gmail.com>
2026-06-09 23:54:49 +0900

    started toying about with react and vite

---
+++ 24-react-with-projects/01basicvite/src/App.jsx
@@ -0,0 +1,12 @@
+import Youtube from "./youtube.jsx"
+
+function App() {
+  return (
+    <>
+      <h1>Look look</h1>
+      <Youtube/>
+    </>
+  )
+}
+
+export default App
---
+++ 24-react-with-projects/01basicvite/src/youtube.jsx
@@ -0,0 +1,7 @@
+function Youtube(){
+  return (
+    <h1>you</h1>
+  )
+}
+
+export default Youtube
