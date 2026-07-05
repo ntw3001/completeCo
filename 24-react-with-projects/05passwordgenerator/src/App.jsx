fa1769b42d56c633bf2438098ba054faa722fdd1
Nick Westbrook <ntw3001@gmail.com>
2026-07-05 23:23:31 +0900

    set up UI for the password generator

--- 24-react-with-projects/05passwordgenerator/src/App.jsx
+++ 24-react-with-projects/05passwordgenerator/src/App.jsx
@@ -8,9 +8,58 @@ function App() {
   const [password, setPassword] = useState("")
 
   return (
-    <>
-      <h1 className="bg-red-400">setup ready</h1>
-    </>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-8 my-8 bg-gray-500">
+      <h1 className="text-white text-center my-3">Password Generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+        type="text"
+        value={password}
+        className="outline-none bg-gray-200 w-full py-1 px-3"
+        placeholder="Password"
+        readOnly
+        />
+        <button className="outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0"
+        >copy</button>
+      </div>
+      <div className="flex items-center gap-x-1">
+        <input
+        type="range"
+        min={6}
+        max={100}
+        value={length}
+        className="cursor-pointer"
+        onChange={(e) => setLength(e.target.value)}
+        name=""
+        id=""
+        />
+        <label htmlFor="length">Length: {length}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+        <input
+        type="checkbox"
+        defaultChecked={numberAllowed}
+        onChange={() => {
+          setNumberAllowed((prev) => !prev)
+        }}
+        name=""
+        id=""
+        />
+        <label htmlFor="number">Numbers</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+        <input
+        type="checkbox"
+        defaultChecked={charAllowed}
+        onChange={() => {
+          setCharAllowed((prev) => !prev)
+        }}
+        name=""
+        id=""
+        />
+        <label htmlFor="charInput">Characters</label>
+      </div>
+
+    </div>
   )
 }
 
