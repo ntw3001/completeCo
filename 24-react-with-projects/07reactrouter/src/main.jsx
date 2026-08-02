4feee686705d8cdb9268152efb38ae99af3f2fc2
Nick Westbrook <ntw3001@gmail.com>
2026-08-02 23:28:52 +0900

    got back to the grind, set up some ROUTES' @

--- 24-react-with-projects/07reactrouter/src/main.jsx
+++ 24-react-with-projects/07reactrouter/src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider,} from 'react-router-dom'
 import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
-
+      <Route path='' element={<Home />}/>
+      <Route path='about' element={<About />} />
     </Route>
   )
 )
