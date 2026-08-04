d3cf5af4608a2be76891d74235effb3aa6a97aa2
Nick Westbrook <ntw3001@gmail.com>
2026-08-04 23:54:03 +0900

    renewed the header

--- 24-react-with-projects/07reactrouter/src/components/Header/Header.jsx
+++ 24-react-with-projects/07reactrouter/src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-      header
-    </div>
+    <header className='shadow sticky z-50 top-0'>
+      <nav className='bg-white border-gray-200 px-4 lg:px-6 py-2.5'>
+        <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
+          <Link to='/' className='flex items-center'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/960px-LEGO_logo.svg.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail&_=20231016092137' className='mr-3 h-12' alt='logo' />
+          </Link>
+        </div>
+      </nav>
+    </header>
   )
 }
 
