fae33cc4615de7bd33c83fbac3e12900c3b0f228
Nick Westbrook <ntw3001@gmail.com>
2026-08-14 21:18:11 +0900

    finally finished that react router project

--- 24-react-with-projects/07reactrouter/src/components/Github/Github.jsx
+++ 24-react-with-projects/07reactrouter/src/components/Github/Github.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
+
+  const data = useLoaderData()
+
   return (
-    <div>
-      Github is yes
+    <div className='bg-gray-600 text-white text-3xl text-center p-5'>
+      Github is yes, {data.followers} followers
+      <img src={data.avatar_url} width={200} alt="" />
     </div>
   )
+
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+  const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+  return response.json()
+}
