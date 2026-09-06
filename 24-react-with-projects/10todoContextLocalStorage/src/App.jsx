e109ec32385ea330a409f74b32ea286bc9d9c551
Nick Westbrook <ntw3001@gmail.com>
2026-09-06 23:57:29 +0900

    doing uh something

--- 24-react-with-projects/10todoContextLocalStorage/src/App.jsx
+++ 24-react-with-projects/10todoContextLocalStorage/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
+import { TodoProvider } from './contexts'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [ {id: Date.now(), ...todo}, ...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === todo.id)))
+  }
 
   return (
-    <>
-      <h1>How do you do</h1>
-    </>
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteToDo, toggleComplete }}>
+      <h1 className='text-3xl font-bold underline'>
+        How do you do
+      </h1>
+    </TodoProvider>
   )
 }
 
