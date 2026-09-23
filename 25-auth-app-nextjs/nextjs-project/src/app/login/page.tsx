898e8711a140a85240c07f11b574905850997e2d
Nick Westbrook <ntw3001@gmail.com>
2026-09-23 23:12:02 +0900

    continue setup for nextjs project

---
+++ 25-auth-app-nextjs/nextjs-project/src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className='flex'>
+      <h1 className='text-center text-green-500  text-2xl'>Login</h1>
+    </div>
+  )
+}
---
+++ 25-auth-app-nextjs/nextjs-project/src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignupPage() {
+  return (
+    <div className='flex'>
+      <h1 className='text-center text-blue-400 text-2xl'>Signup</h1>
+    </div>
+  )
+}
